7e75eba4425076492f8e8faabbe6211679c2f297
Moctor <271448116@qq.com>
2020-06-25 15:45:36 +0800

    支持登录的船新版本,可自行登录获取token避免多人使用同一token导致账号异常或封号.获取到的token默认储存在token.txt内,方便下次直接使用.

--- 学小易/Xuexiaoyi.py
+++ 学小易/Xuexiaoyi.py
@@ -1,9 +1,11 @@
 import requests
-def getanswer(text):                #一个简单的post请求获取答案内容
+def getanswer(text,token):                #一个简单的post请求获取答案内容
+    if (token==False):
+        return False
     headers={
         "Host":"app.51xuexiaoyi.com",
-        "token":"你自己登录时的token",
-        "device":"自己的device标识",
+        "token":token,
+        "device":"Auhqehd3s6Ml6mXky_5dV-Uv4zsdXeUYY7wKFktkH1ag",
         "platform":"android",
         "app-version": "1.0.6",
         "t":"1592904062239",
@@ -26,19 +28,53 @@ def outanswer(li):
     if isinstance(li,list):         #由于获取成功返回的是列表,所以这里先判断是不是列表进而得知是否成功
         for i in li:                #若成功,则可通过迭代的方式遍历列表
             print(" ")
-            print('问题:%s'%i['q'])           #列表中的每个元素都是一个字典,这里通过格式化输出把字典里需要的内容输出
+            print('问题:')           #列表中的每个元素都是一个字典,这里通过格式化输出把字典里需要的内容输出
+            print(i['q'])
+            print(" ")              #输出空行为了看起来更方便
+            print('答案:')
+            print(i['a'])
             print(" ")
-            print('答案:%s'%i['a'])
             print('*'*50)
     else:
-        print(li)
+        print(li)                   #若参数不为列表,则返回的是错误信息,所以这里可以打印错误信息
+def login():
+    username=input("请输入账号:")
+    password=input("请输入密码:")
+    url="https://app.51xuexiaoyi.com/api/v1/login?username="+username+"&password="+password
+    headers={
+        "Host":"app.51xuexiaoyi.com",
+        "device":"Auhqehd3s6Ml6mXky_5dV-Uv4zsdXeUYY7wKFktkH1ag",
+        "platform":"android",
+        "app-version":"1.0.6",
+        "t":"1593008524987",
+        "s":"53029a76022a2f4a52c11f08a84759c0",
+        "Content-Type":"application/json; charset=utf-8",
+        "Accept-Encoding":"gzip",
+        "User-Agent":"okhttp/3.11.0"
+    }
+    res=requests.post(url,headers=headers).json()
+    if (res['code']==200):
+        f=open('token.txt','w+')
+        f.write(res['data']['api_token'])
+        f.close()
+        print("登陆成功!")
+        return res['data']['api_token']
+    else:
+        return res['msg']
 def main():
-    print('Welcome to "学不易" 输入"exit"或点"x"即可退出')     #程序运行逻辑
-
+    print('Welcome to "学不易" 输入"exit"或点"x"即可退出')
+    try:
+        f=open('token.txt','w+')
+        token=f.read()
+        f.close()
+        if(token==""):
+            token=login()
+    except:
+        token=login()
     while True:
         questions=input("请输入题目:")
         if len(questions)>=6:
-            res = getanswer(questions)
+            res = getanswer(questions,token)
             outanswer(res)
         elif questions == 'exit':
             break
@@ -47,5 +83,6 @@ def main():
     exit()
 if __name__ == "__main__":
      main()
+
 
 
