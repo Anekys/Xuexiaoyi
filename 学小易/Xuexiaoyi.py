ff6b6b62044345e6e6141f52f4170848a832ae5a
Moctor <271448116@qq.com>
2020-06-28 17:29:22 +0800

    增加图片搜题功能 通过调用百度云的OCR文字识别提取文字,实现以图搜题功能.支持目前主流的大部分图片格式,如:PNG,JPG,BMP,JPEG等.

--- 学小易/Xuexiaoyi.py
+++ 学小易/Xuexiaoyi.py
@@ -1,4 +1,12 @@
 import requests
+import os
+from aip import AipOcr
+#这里调用了百度云智能大脑的图像识别API,以下三个Key均为调用API需要,可在你自己的百度云管理界面查询到
+token=""                            #申明一个全局变量Token
+APP_ID='16593668'                           #你的百度云APP_ID
+APP_KEY='FhqfbBANUQ9H1Q1pj8tS97QH'          #你的百度云Key
+SECRET_KEY='VtPljGlCiXym52xRmiZCoUNX4Z4dsmWT'   #你的百度云Secret_Key(老版本百度云的 Access Key)
+client=AipOcr(APP_ID,APP_KEY,SECRET_KEY)
 def getanswer(text,token):                #一个简单的post请求获取答案内容
     if (token==False):
         return False
@@ -37,7 +45,7 @@ def outanswer(li):
             print('*'*50)
     else:
         print(li)                   #若参数不为列表,则返回的是错误信息,所以这里可以打印错误信息
-def login():                                                #登录函数(获取账号的token)
+def login():            #调用API获取账号的token
     username=input("请输入账号:")
     password=input("请输入密码:")
     url="https://app.51xuexiaoyi.com/api/v1/login?username="+username+"&password="+password
@@ -53,36 +61,58 @@ def login():                                                #登录函数(获取
         "User-Agent":"okhttp/3.11.0"
     }
     res=requests.post(url,headers=headers).json()
-    if (res['code']==200):                                          #判断是否登录成功(获取到token)
-        f=open('token.txt','w+')                                    #若成功,则储存在token.txt中,方便下次使用
+    if (res['code']==200):
+        f=open('token.txt','w+')
         f.write(res['data']['api_token'])
         f.close()
         print("登陆成功!")
         return res['data']['api_token']
     else:
         return res['msg']
+def OCR(imagename):     #参数为图片文件路径,函数的功能为获取图片数据,并通过百度云的OCR文字识别提取出图片中的文字.
+    with open(imagename, 'rb') as fp:
+        image=fp.read()
+        retlist=client.basicGeneral(image)
+        text=""                                     #由于百度云的OCR识别将结果作为一个列表进行返回,所以这里通过for循环将列表迭代为一个文本
+        for i in retlist['words_result']:
+            text=text+i['words']
+        return text
+def Ocrmode():                          #OCR文字识别模式,可将题目截图到任意目录,输入图片绝对路径一键进行题目提取及答案获取的操作
+    while True:
+        print("请输入图片路径:")
+        path=input("ocrmode>>>")
+        if(os.path.exists(path)):           #检测图片是否存在,若存在,则进行文字识别并查询答案
+            text=OCR(path)
+            res=getanswer(text,token)
+            outanswer(res)
+        elif path=="exocr" or "EXOCR":
+            return
+        else:
+            print("图片不存在或没有访问权限!")
+
 def main():
     print('Welcome to "学不易" 输入"exit"或点"x"即可退出')
+    print('直接输入题目即可获取答案,也可输入"OCR"进入图片识别模式')
     try:
-        f=open('token.txt','w+')
+        f=open('token.txt','r+')
         token=f.read()
         f.close()
         if(token==""):
             token=login()
+            print("这里是空")
     except:
         token=login()
     while True:
-        questions=input("请输入题目:")
+        questions=input(">>>")
         if len(questions)>=6:
             res = getanswer(questions,token)
             outanswer(res)
         elif questions == 'exit':
             break
+        elif questions=="ocr" or "OCR":
+            Ocrmode()
         else:
             print("题目必须大于6个字符")
     exit()
 if __name__ == "__main__":
      main()
-
-
-
